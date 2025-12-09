58bb5b7ecc740a2a28104c29ce8df015fb04d151
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2025-12-09 13:27:28 +0200

    config eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,4 +4,13 @@ export default antfu({
   type: 'lib',
   gitignore: true,
   ignores: ['**/test/*.json'],
+  stylistic: {
+    indent: 2,
+    quotes: 'single',
+  },
+  formatters: true,
+  rules: {
+    'unicorn/prefer-node-protocol': 'off',
+  },
 })
+
